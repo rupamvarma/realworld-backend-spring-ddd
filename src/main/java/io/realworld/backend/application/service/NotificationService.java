cea5ccbe797ad779e0150ef3c3fc7c7c42b6a7b5
rupamvarma <rupam.varma@zemosolabs.com>
2023-03-15 13:59:48 +0530

    be-get-notifications

--- src/main/java/io/realworld/backend/application/service/NotificationService.java
+++ src/main/java/io/realworld/backend/application/service/NotificationService.java
@@ -15,8 +15,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
